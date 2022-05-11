92667c73b04e3705fdd70bde86c645414c815d3f
sinancalbiyik <101750952+sinancalbiyik@users.noreply.github.com>
2022-05-11 11:55:20 +0300

    ilk denemem

---
+++ ilk/ilk/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ilk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("selam");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("oldula");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi = 10;
+            int sayi2 = 5;
+            MessageBox.Show((sayi+sayi2).ToString());
+
+        }
+    }
+}
